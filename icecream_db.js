86f62d390086cbe7f1467c3ba106f4378db14cc5
Whitley Blaine <whitleyblaine@gmail.com>
2016-07-13 20:13:57 -0400

    8:13

---
+++ icecream_db.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root", //Your username
+  password: "E$*G8Y1el6rn0AshUb6YY5Nm6F62103$", //Your password
+  database: "icecream_db"
+})
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+})
